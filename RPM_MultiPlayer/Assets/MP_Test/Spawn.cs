1eb9c5b9b2d84ad6f354332cfc8d0a9b1bbbb7bb
Hayoung <hayoung200585@gmail.com>
2024-02-19 16:34:03 +0900

    spawn 1 player prefab
    run hy 3 spawn -> and then load to hy 3 spawn 1

--- RPM_MultiPlayer/Assets/MP_Test/Spawn.cs
+++ RPM_MultiPlayer/Assets/MP_Test/Spawn.cs
@@ -2,16 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(NetworkObject))]
 public class Spawn : NetworkBehaviour
 {
+    [SerializeField] private GameObject playerPrefab;
+    
+    public static string InputName = string.Empty;
+    //private var networkPrefabList = null;
+
+    public void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
 
     public override void OnNetworkSpawn()
     {
-        if(IsServer)
-        {
+        NetworkManager.Singleton.SceneManager.OnLoadEventCompleted += SceneLoaded;
+    }
 
+    private void SceneLoaded(string sceneName, LoadSceneMode loadSceneMode, List<ulong> clientsCompleted, List<ulong> clientsTimedOut)
+    {
+        if (IsHost && sceneName == "AvatarControl_HY 3_Spawn 1")
+        {
+            foreach (var id in clientsCompleted)
+            {
+                var player = Instantiate(playerPrefab);
+                player.GetComponent<NetworkObject>().SpawnAsPlayerObject(id, true);
+                
+            }
         }
+    }
+
+    private void FindAvatar()
+    {
+        //var info = networkPrefabList.Find(a => a.Prefab.name == InputName);
+
+    }
+
+    public void Initialize()
+    {
+        //networkPrefabList = NetworkManager.NetworkConfig.Prefabs.NetworkPrefabsLists.Find(l => l.name == "Multi Play XR_Avatar Repository NetworkPrefabsList").PrefabList.ToList();
     }
 }
